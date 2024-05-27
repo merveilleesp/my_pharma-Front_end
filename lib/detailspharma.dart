ff04513103cf1a626b43574b9dea5e90b75782ce
merveilleesp <101706242+merveilleesp@users.noreply.github.com>
2024-05-27 09:13:32 +0100

    27/05/24

--- lib/detailspharma.dart
+++ lib/detailspharma.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 class DetailsPharmacies extends StatelessWidget {
   final String nomPharmacie = "";
-  final String nomDocteur= "";
-  final String localisation= "";
-  final String assurances= "";
-  final String contacts= "";
-  final String heuresOuverture= "";
-  final String moyensPaiements= "";
+  final String nomDocteur = "";
+  final String localisation = "";
+  final String assurances = "";
+  final String contacts = "";
+  final String heuresOuverture = "";
+  final String moyensPaiements = "";
   final int id;
 
   DetailsPharmacies({
@@ -21,64 +21,113 @@ class DetailsPharmacies extends StatelessWidget {
         title: Text('Nom de l\'Appli'),
         backgroundColor: Colors.teal,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.all(16.0),
-                color: Colors.grey[300],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      id.toString(),
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Dr. $id',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16.0),
+              color: Colors.grey[300],
+              width: double.infinity, // Prendre toute la largeur disponible
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                  'assets/Logopharma2.png',
+                      width: 100,
                 ),
+                  SizedBox(height: 8),
+                  Text(
+                    id.toString(),
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color :Colors.teal
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Dr. $id',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.teal
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              InformationTile(
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, top: 16, right:35, bottom: 16),
+              child: InformationTile(
                 icon: Icons.location_on,
                 title: 'Localisation',
                 content: localisation,
+                titleStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.teal,
+                ),
               ),
-              InformationTile(
+            ),
+            Divider(height: 0.5, color: Colors.grey[300]),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, top: 16, right:35, bottom: 16),
+              child: InformationTile(
                 icon: Icons.security,
                 title: 'Assurances',
                 content: assurances,
+                titleStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.teal,
+                ),
               ),
-              InformationTile(
+            ),
+            Divider(height: 0.5, color: Colors.grey[300]),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, top: 16, right:35, bottom: 16),
+              child: InformationTile(
                 icon: Icons.phone,
                 title: 'Contacts',
                 content: contacts,
+                titleStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.teal,
+                ),
               ),
-              InformationTile(
+            ),
+            Divider(height: 0.5, color: Colors.grey[300]),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, top: 16, right:35, bottom: 16),
+              child: InformationTile(
                 icon: Icons.access_time,
                 title: 'Heures d\'ouverture',
                 content: heuresOuverture,
+                titleStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.teal,
+                ),
               ),
-              InformationTile(
+            ),
+            Divider(height: 0.5, color: Colors.grey[300]),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, top: 16, right:35, bottom: 16),
+              child: InformationTile(
                 icon: Icons.payment,
                 title: 'Moyens de paiements',
                 content: moyensPaiements,
+                titleStyle: TextStyle(
+                  fontSize: 18,
+                  color: Colors.teal,
+                ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -89,40 +138,36 @@ class InformationTile extends StatelessWidget {
   final IconData icon;
   final String title;
   final String content;
+  final TextStyle titleStyle;
 
   InformationTile({
     required this.icon,
     required this.title,
     required this.content,
+    required this.titleStyle,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(icon, color: Colors.teal),
-          SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(content),
-              ],
-            ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Icon(icon, color: Colors.teal),
+        SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: titleStyle,
+              ),
+              SizedBox(height: 5),
+              Text(content),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
